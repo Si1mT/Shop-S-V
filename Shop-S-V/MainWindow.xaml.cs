479b99ab9361c1d8678ff58d404c2bee58856791
Virk0 <virko.veelmaa@tptlive.ee>
2018-03-22 09:19:17 +0200

    ui

--- Shop-S-V/MainWindow.xaml.cs
+++ Shop-S-V/MainWindow.xaml.cs
@@ -20,9 +20,56 @@ namespace Shop_S_V
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            textbox1.Visibility = Visibility.Hidden;
+            textbox2.Visibility = Visibility.Hidden;
+            textbox3.Visibility = Visibility.Hidden;
+            Toode.Visibility = Visibility.Hidden;
+            Hind.Visibility = Visibility.Hidden;
+            Kogus.Visibility = Visibility.Hidden;
+            Kassa.Visibility = Visibility.Visible;
+            Lisa_Toode.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Visibility = Visibility.Visible;
+            textbox2.Visibility = Visibility.Visible;
+            textbox3.Visibility = Visibility.Visible;
+            Toode.Visibility = Visibility.Visible;
+            Hind.Visibility = Visibility.Visible;
+            Kogus.Visibility = Visibility.Visible;
+            Lisa.Visibility = Visibility.Hidden;
+            Kassa.Visibility = Visibility.Hidden;
+            Lisa_Toode.Visibility = Visibility.Visible;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Restart(object sender, RoutedEventArgs e)
+        {
+            textbox1.Visibility = Visibility.Hidden;
+            textbox2.Visibility = Visibility.Hidden;
+            textbox3.Visibility = Visibility.Hidden;
+            Toode.Visibility = Visibility.Hidden;
+            Hind.Visibility = Visibility.Hidden;
+            Kogus.Visibility = Visibility.Hidden;
+            Kassa.Visibility = Visibility.Visible;
+            Lisa_Toode.Visibility = Visibility.Hidden;
+            Lisa.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_Kassa(object sender, RoutedEventArgs e)
+        {
+            Lisa_Toode.Visibility = Visibility.Hidden;
+            Kassa.Visibility = Visibility.Hidden;
+            Lisa.Visibility = Visibility.Hidden;
         }
     }
 }
